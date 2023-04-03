78f0f4e449234d35b0713599e021ee12eefb0921
Tatjana Royal <post@scubbo.de>
2023-04-03 17:22:39 +0200

    chromatic setup

--- fm-library/.storybook/preview.js
+++ fm-library/.storybook/preview.js
@@ -9,6 +9,19 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },  
+  backgrounds: {
+    default: 'hell',
+    values: [
+      {
+        name: 'hell',
+        value: '#C0D1E1',
+      },
+      {
+        name: 'dunkel',
+        value: '#203864',
+      },
+    ],
   },
   docs: { inlineStories: true },
 }
--- fm-library/src/app/app.module.ts
+++ fm-library/src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CounterComponent } from './elements/counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FontAwesomeModule,
     BrowserAnimationsModule
   ],
   providers: [],
